e3c5b0c4618bc17592ac624ee9af86f6c5731509
Jonatan Sombié <jonatan.sombie@gmail.com>
2025-01-21 13:46:03 +0100

    Initial commit

---
+++ test/hereya-ec2-web-deploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HereyaEc2WebDeploy from '../lib/hereya-ec2-web-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hereya-ec2-web-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HereyaEc2WebDeploy.HereyaEc2WebDeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
